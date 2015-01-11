e6ca0f9a6ea817bbf439e2d90737ab47d3abc4dd
ymangu163 <ymangu@163.com>
2015-01-11 18:30:39 +0800

    添加底部菜单；Inflater 的 3种方式

--- src/com/ymangu/mynews/MainActivity.java
+++ src/com/ymangu/mynews/MainActivity.java
@@ -4,6 +4,7 @@ package com.ymangu.mynews;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.ymangu.mynews.fragment.Fragment1;
+import com.ymangu.mynews.fragment.HomeFragment;
 import com.ymangu.mynews.fragment.MenuFragment;
 import com.ymangu.mynews.fragment.RightMenuFragment;
 
@@ -51,13 +52,14 @@ public class MainActivity extends SlidingFragmentActivity {
 		sm = getSlidingMenu(); // 1 得到滑动菜单
 		// 2设置滑动菜单是在左边出来还是右边出来
 		// 参数可以设置左边LEFT，也可以设置右边RIGHT ，还能设置左右LEFT_RIGHT
-		sm.setMode(SlidingMenu.LEFT_RIGHT);
+		sm.setMode(SlidingMenu.LEFT);
 		// 3设置滑动菜单出来之后，内容页，显示的剩余宽度
 		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
 		// 4 设置滑动菜单的阴影 设置阴影，阴影需要在开始的时候，特别暗，慢慢的变淡
 		sm.setShadowDrawable(R.drawable.shadow);
 		// 5 设置阴影的宽度
 		sm.setShadowWidth(R.dimen.shadow_width);
+
 		// 6 设置滑动菜单的范围
 		// 第一个参数 SlidingMenu.TOUCHMODE_FULLSCREEN 可以全屏滑动
 		// 第二个参数 SlidingMenu.TOUCHMODE_MARGIN 只能在边沿滑动
@@ -78,11 +80,17 @@ public class MainActivity extends SlidingFragmentActivity {
 				/**
 				 * 下面的代码是右边侧滑
 				 */
-				sm.setSecondaryMenu(R.layout.right_menu);
-				sm.setSecondaryShadowDrawable(R.drawable.shadowright);
-				RightMenuFragment rightMenuFragment = new RightMenuFragment();
-				getSupportFragmentManager().beginTransaction().replace(R.id.right_menu_frame, rightMenuFragment).commit();
-	
+//				sm.setSecondaryMenu(R.layout.right_menu);
+//				sm.setSecondaryShadowDrawable(R.drawable.shadowright);
+//				RightMenuFragment rightMenuFragment = new RightMenuFragment();
+//				getSupportFragmentManager().beginTransaction().replace(R.id.right_menu_frame, rightMenuFragment).commit();
+
+				// 内容页
+				HomeFragment homeFragment = new HomeFragment();
+				getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, homeFragment, "Home").commit();
+				
+				
+				
 				
 	}
 	
--- src/com/ymangu/mynews/fragment/MenuFragment.java
+++ src/com/ymangu/mynews/fragment/MenuFragment.java
@@ -9,9 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.ymangu.mynews.MainActivity;
 import com.ymangu.mynews.R;
 
 /*
@@ -21,7 +24,7 @@ import com.ymangu.mynews.R;
  * 4.  Fragment 生命周期：
  * 		onCreate() --->onCreateView() -->onActivityCreated().
  */
-public class MenuFragment extends Fragment {
+public class MenuFragment extends Fragment implements OnItemClickListener {
 
 	private View view;
 
@@ -41,6 +44,8 @@ public class MenuFragment extends Fragment {
 				android.R.layout.simple_list_item_1, android.R.id.text1,
 				initData());
 		list_view.setAdapter(adapter);
+		
+		list_view.setOnItemClickListener(this);  // item点击事件
 		
 	}
 
@@ -66,6 +71,54 @@ public class MenuFragment extends Fragment {
 		view = LayoutInflater.from(getActivity()).inflate(R.layout.menu_list_view, null);	
 		
 		return view;   // 把View 返回回去
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		//点击的时候来回切换，建5个Fragment, 点击的时候就创建出来
+		
+		Fragment f  = null;
+		switch (position) {
+		case 0:
+			f  =new Fragment1();    
+			break;
+			
+		case 1:
+			f  =new Fragment2();
+			break;
+		case 2:
+			f  =new Fragment3();
+			break;
+		case 3:
+			f  =new Fragment4();
+			break;
+		case 4:
+			f  =new Fragment5();
+			break;
+		}
+		// 通过回调，来实现通信
+		 switchFragment(f);
+	}
+
+	/*
+	 * . 点击之后，去更新 contentFragment的内容，怎么实现呢？
+	 *  通过 class B--MenuFragment 的 C 方法，去调用  class A -- MainActivity的 D方法:
+	 *   所以 MainActivity 要暴露一个 D 方法
+	 */
+	private void switchFragment(Fragment f) {
+		if (f != null) {
+			/*
+			 * . getActivity() 拿到上下文， getActivity() instanceof MainActivity 这样就回到 MainActivity 去了
+			 *  这样就比定义接口省事得多
+			 */
+			if (getActivity() instanceof MainActivity) {
+				((MainActivity) getActivity()).switchFragment(f);
+
+			}
+
+		}
+
 	}
 
 }
