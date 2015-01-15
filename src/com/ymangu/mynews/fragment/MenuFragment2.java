833b68a7fa4af6aed0e0bb311cdb2326f60ee752
ymangu163 <ymangu@163.com>
2015-01-15 22:58:59 +0800

    ① 提高性能 ②打标记，点击的是哪个Item. 点击事件中去改变背景；

--- src/com/ymangu/mynews/fragment/MenuFragment2.java
+++ src/com/ymangu/mynews/fragment/MenuFragment2.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -21,7 +24,7 @@ import com.ymangu.mynews.base.BaseFragment;
  * . 因为多个Page 对应的菜单不一样，最简单的方法就是 建对应的 xxMenuFragment 去替换SlidingMenu的id;
  *   还有更简洁的方法是 建立多个 ListView，迭在同一个FrameLayout 中，控件哪个 Visible 就可以。
  */
-public class MenuFragment2 extends BaseFragment {
+public class MenuFragment2 extends BaseFragment implements OnItemClickListener {
 	@ViewInject(R.id.lv_menu_news_center)
 	private ListView lv_menu_news_center;
 	
@@ -29,7 +32,8 @@ public class MenuFragment2 extends BaseFragment {
 	public View initView(LayoutInflater inflater) {
 		View view = inflater.inflate(R.layout.left_menu_layout, null);
 		ViewUtils.inject(this, view);   // 在 Fragment 中注入view
-		
+		// 设置 ListView Item的点击事件
+		lv_menu_news_center.setOnItemClickListener(this);
 		return view;
 	}
 
@@ -39,30 +43,41 @@ public class MenuFragment2 extends BaseFragment {
 	}
 
 	private List<String> menuList=new ArrayList<String>();
+	private MenuAdapter adapter;
 	/*
 	 * . 初始化菜单数据
 	 */
 	public void initMenu(List<String> menuNewCenterList) {
 		menuList.clear();   // 先清空一下，保证它没有杂质
 		menuList.addAll(menuNewCenterList);
-		
-		// 拿到数据源之后，用 Adapter 去适配 ListView.
-		MenuAdapter adapter=new MenuAdapter(context,menuList);
-		lv_menu_news_center.setAdapter(adapter);
-		
+		// 提高性能
+		if(adapter==null){
+			adapter = new MenuAdapter(context,menuList);
+			lv_menu_news_center.setAdapter(adapter);			
+		}else{
+			adapter.notifyDataSetChanged();   //如果存在的话，只需要刷新一下就好了
+		}
+		adapter.setCurPosition(0);  // 调用指定位置的方法
 	}
 
 	class MenuAdapter extends BaseAdapter{
 		private Context context;
 		private List<String> menuList;
+		private int selectedPosition=0; // 打个标记，选中的项，默认选中第1项
 		public MenuAdapter(Context context, List<String> menuList) {
 			this.context=context;
 			this.menuList=menuList;
 		}
-
+		/*
+		 * . 加个方法，打个标记  记录选中的位置
+		 */
+		public void  setCurPosition(int position){
+			this.selectedPosition=position;			
+			notifyDataSetChanged();   //记住要去刷新
+		}
+		
 		@Override
 		public int getCount() {
-			// TODO Auto-generated method stub
 			return menuList.size();
 		}
 
@@ -85,9 +100,34 @@ public class MenuFragment2 extends BaseFragment {
 			ImageView iv = (ImageView) convertView	.findViewById(R.id.iv_menu_item);
 			tv.setText(menuList.get(position));
 			
+			// view 渲染的时候，如果是选中的项就变成不一样的颜色
+			if (selectedPosition == position) {
+				convertView.setSelected(true);
+				convertView.setPressed(true);
+				// 增加背景
+				convertView.setBackgroundResource(R.drawable.menu_item_bg_select);
+				tv.setTextColor(context.getResources().getColor(
+						R.color.menu_item_text_color));
+				iv.setBackgroundResource(R.drawable.menu_arr_select);
+			} else {
+				convertView.setSelected(false);
+				convertView.setPressed(false);
+				convertView.setBackgroundColor(Color.TRANSPARENT);
+				iv.setBackgroundResource(R.drawable.menu_arr_normal);
+				tv.setTextColor(context.getResources().getColor(R.color.white));
+			}
 			
 			return convertView;
 		}
+		
+		
+	}
+	
+   // ListView 的 Item 点击事件
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		adapter.setCurPosition(position);
 		
 		
 	}
