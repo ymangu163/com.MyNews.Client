c6191424fd501bfad3d09edf0788fc0b755ac575
ymangu163 <ymangu@163.com>
2015-01-21 23:33:35 +0800

    添加了 极光推送

--- src/com/ymangu/mynews/MainActivity.java
+++ src/com/ymangu/mynews/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ymangu.mynews;
 
 
+import cn.jpush.android.api.CustomPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
+
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.ymangu.mynews.fragment.Fragment1;
@@ -91,8 +94,13 @@ public class MainActivity extends SlidingFragmentActivity {
 				getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, homeFragment, "Home").commit();
 				
 				
-				
-				
+		// 极光自定义通知样式
+		CustomPushNotificationBuilder builder = new CustomPushNotificationBuilder(MainActivity.this,
+		    R.layout.customer_notitfication_layout, R.id.icon, R.id.title, R.id.text);  // 指定定制的 Notification Layout
+		builder.statusBarDrawable = R.drawable.icon_share;      // 指定最顶层状态栏小图标
+		builder.layoutIconDrawable = R.drawable.govaffairs_press2;   // 指定下拉状态栏时显示的通知图标
+		JPushInterface.setPushNotificationBuilder(2, builder);
+						
 	}
 	
 	/**
@@ -116,6 +124,20 @@ public class MainActivity extends SlidingFragmentActivity {
 		//自动切换
 		sm.toggle();		
 		
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		JPushInterface.onResume(this);
+		
+		
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		JPushInterface.onPause(this);
 	}
 	
 }
